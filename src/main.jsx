b0b083e090d93e0968d6c28a14d9b4bd2955609e
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2026-04-13 11:37:12 +0100

    Leves correções no main com o QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={new QueryClient()}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
     
   </StrictMode>,
 )
