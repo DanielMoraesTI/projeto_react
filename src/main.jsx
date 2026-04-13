a88fde78615f2f6178c734fe00826ddaab1cdf56
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2026-04-13 15:20:52 +0100

    Main com as Routes iniciais criadas

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,23 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Dashboard from './pages/Dashboard.jsx'
+import AddTransaction from './pages/AddTransaction.jsx'
+import History from './pages/History.jsx'
+import Settings from './pages/Settings.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
     <QueryClientProvider client={new QueryClient()}>
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add-transaction" element={<AddTransaction />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
       </BrowserRouter>
     </QueryClientProvider>
     
