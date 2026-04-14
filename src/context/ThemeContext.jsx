082f0bcf40da2281419aba266010af2799922768
Tiago Machado <timac1997@gmail.com>
2026-04-14 17:08:26 +0100

    escuro default preset change

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useState } from 'react'
 export const ThemeContext = createContext()
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState('claro')
+  const [theme, setTheme] = useState('escuro')
 
   function toggleTheme() {
     setTheme(prev => prev === 'claro' ? 'escuro' : 'claro')
