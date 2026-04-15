40a39df686aa553db14f19a8979399dc2590b5e8
Tiago Machado <timac1997@gmail.com>
2026-04-15 10:39:40 +0100

    user name adicionado em summary

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -4,13 +4,16 @@ import { PreferencesContext } from "../context/PreferencesContext";
 // O componente Summary recebe os valores de balance, income e expense como props. Ele renderiza um resumo financeiro, exibindo o saldo atual, total de receitas e total de despesas. O saldo é formatado com duas casas decimais e a cor do valor é definida como verde para saldos positivos e vermelho para saldos negativos. As receitas e despesas também são formatadas com duas casas decimais e exibidas em cores correspondentes (verde para receitas e vermelho para despesas). O layout é organizado em uma estrutura de cartões para destacar as informações financeiras.
 
 export function Summary({ balance, income, expense }) {
-  const { currency } = useContext(PreferencesContext);
+  const { currency, userName } = useContext(PreferencesContext);
 
   const format = (value) =>
     new Intl.NumberFormat("pt-PT", { style: "currency", currency }).format(value);
 
   return (
-    <div className="summary">
+    <>
+      {userName && <p>Olá, {userName}!</p>}
+
+      <div className="summary">
       <div className="card summary-main-card">
         <h3>Saldo Atual</h3>
         <p className={balance >= 0 ? "positive" : "negative"}>
@@ -30,5 +33,6 @@ export function Summary({ balance, income, expense }) {
         </div>
       </div>
     </div>
+    </>
   );
 }
